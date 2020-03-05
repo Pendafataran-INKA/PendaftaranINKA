fd6efa01e68ea205cc4fa60482fc0edaf963963a
Rifqi Hakim <rifqihakim99@gmail.com>
2020-03-05 09:52:34 +0700

    no message

--- application/views/user_Dashboard.php
+++ application/views/user_Dashboard.php
@@ -66,26 +66,13 @@
 
       <nav class="nav-menu float-right d-none d-lg-block">
         <ul>
-          <li class="active"><a href="#header">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li class="drop-down"><a href="">Drop Down</a>
+          <li class="active"><a href="#header">Beranda</a></li>
+          <li><a href="#about">Pengumuman</a></li>
+          <li><a href="#services">Dokumen Pernyataan</a></li>
+          <li class="drop-down"><a href="">Alur</a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="drop-down"><a href="#">Drop Down 2</a>
-                <ul>
-                  <li><a href="<?= base_url('coba'); ?>">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-              <li><a href="#">Drop Down 5</a></li>
+              <li><a href="#">Pendaftaran Prakerin (SMK)</a></li>
+              <li><a href="#">Pendaftaran PKL</a></li>
             </ul>
           </li>
           <li><a href="<?= base_url('admin_Dashboard'); ?>">Login</a></li>
